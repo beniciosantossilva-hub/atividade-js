b6c1f4a7ac8915e381b7c7b0cee65cb903acb6b7
Benicioxp <benicio.santos.silva@escola.pr.gov.br>
2026-06-25 11:34:31 -0300

    Adicionado mais funções no HTML, CSS e JS
    + a alteração de algumas cores de fundo

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function revelarMensagem() {
-    document.getElementById("mensagem").innerText = "Olá Mundo! Aprendi JS"
+    document.getElementById("mensagem").innerText = "Olá Mundo! Alterei a mensagem chamando uma função JS"
+}
+
+function mostrarNome() {
+    let nome = document.getElementById("nomeUsuario").value;
+    document.getElementById("resultadonome").innerText = "Bem Vindo, " + nome + "!";
 }
